0a9da2b5a81eae7f722e4e41987f77a668dee406
Arun <arun@webandcrafts.in>
2022-08-28 14:34:07 +0530

    added multiple rectangles

--- app.js
+++ app.js
@@ -7,15 +7,30 @@ function draw() {}
 
 async function startDrawing() {
   drawEllipse(400, 400, 400, 400, "#FFFF00")
-  await sleep(1000)
+  await sleep(300)
 
   fill(color("red"))
   rect(260, 260, 280, 280)
 
-  await sleep(1000)
+  await sleep(300)
+  fill(color("orange"))
+  quad(200, 400, 400, 200, 600, 400, 400, 600)
 
-  fill(color("green"))
-  triangle(260, 450, 400, 260, 540, 450)
+  fill(color("orange"))
+  await sleep(300)
+  quad(230, 300, 510, 230, 580, 510, 300, 580)
+
+  fill(color("orange"))
+  await sleep(300)
+  quad(580, 310, 510, 580, 230, 510, 310, 230)
+
+  // await sleep(300)
+  // fill(color("green"))
+  // triangle(260, 450, 400, 260, 540, 450)
+
+  // await sleep(300)
+  // fill(color("green"))
+  // triangle(400, 550, 260, 360, 540, 360)
 }
 
 function drawEllipse(xCoordinate, yCoordinate, width, height, colorHex) {
